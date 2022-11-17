469414befe3323f789e19ea8216d3a036506f0e1
Bhuvanesh Trivedi <bhuvaneshtrivedi59@gmail.com>
2022-11-17 20:53:55 +0530

    deployment commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const shortId = require('shortid')
 const ShortUrl = require('./connection')
 const app = express()
+const path = require('path')
 require('dotenv').config({path:'/.env'})
 
 app.use(express.json());
@@ -12,14 +13,6 @@ app.use(function (req, res, next) {
     next()
 });
 
-// ----------------- deployment ---------------------------
-
-if(process.env.NODE_ENV === "production"){__dirname = path.resolve()
-app.use(express.static(path.join(__dirname,'./frontend/build')))
-app.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-})}
-// ----------------- deployment ---------------------------
 
 mongoose.connect(`mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.7wofpmp.mongodb.net/?retryWrites=true&w=majority`, {
   useNewUrlParser: true, useUnifiedTopology: true
@@ -30,9 +23,17 @@ const port = process.env.PORT || 5000
 app.use(express.urlencoded({ extended: false }))
 
 app.listen(port,()=>{
-    console.log(`server listening on port ${port}`);
+  console.log(`server listening on port ${port}`);
 })
 
+// ----------------- deployment ---------------------------
+
+if(process.env.NODE_ENV === "production"){__dirname = path.resolve()
+app.use(express.static(path.join(__dirname,'./frontend/build')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+})}
+// ----------------- deployment ---------------------------
 
 app.post('/',async(req,res)=>{
   const init_url = req.body.inurl
